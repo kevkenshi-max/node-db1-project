a23574754a7bbcd92d37a48ca302c0f0c9970e8f
Kevin Lee <kevin-lee@lambdastudents.comgit config --global user.email kevkenshi@gmail.com>
2022-03-21 14:46:47 -0700

    kevin-lee

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,30 @@
+const db = require('../../data/db-config')
+
 const getAll = () => {
-  // DO YOUR MAGIC
+  // select * from accounts;
+  return db('accounts')
 }
 
 const getById = id => {
-  // DO YOUR MAGIC
+  // select * from accounts where id = 1;
+  return db('accounts').where('id', id).first()
 }
 
-const create = account => {
-  // DO YOUR MAGIC
+const create = async account => {
+  // insert into accounts (name, budget) values ('foo', 1000); <-- ; is not an option in SQL
+  const [id] = await db('accounts').insert(account)
+  return getById(id)
 }
 
-const updateById = (id, account) => {
-  // DO YOUR MAGIC
+const updateById = async (id, account) => {
+  // update accounts set name='foo', budget=1000 where id = 1;
+  await db('accounts').where('id', id).update(account)
+  return getById(id)
 }
 
 const deleteById = id => {
-  // DO YOUR MAGIC
+  // delete from accounts where id = 1;
+  return db('accounts').where('id', id).del()
 }
 
 module.exports = {
@@ -25,3 +34,11 @@ module.exports = {
   updateById,
   deleteById,
 }
+
+// db('foo-table')      --> returns a promise that resolves to an **array** with all records in the table
+// db('foo-table').where({ role: 'Student', active: true })      --> resolves to an **array** of all records that satisfy the where
+// db('foo-table').where('name', 'Mary')      --> is an alternative for when there is just one where condition
+// db('foo-table').where('id', 7).first()      --> will resolve to the **record** we want (if the id is unique for a table) or undefined
+// db('foo-table').insert({ bar: 'baz' })      --> resolves to an **array** containing the **ids of the records** inserted into the table
+// db('foo-table').where('id', id).update({ bar: 'new bar' })      --> resolves to the **number of records** affected by the update
+// db('foo-table').where('id', id).delete()      --> resolves to the **number of records** affected by the delete
